3f3f888be59083bbdc0f144490a1816e507ac4b7
Manuel Meneses <manuelmeneses7200@gmail.com>
2025-05-17 15:14:38 -0500

    feat: :sparkles: Boton de retroceso
    Se ha añadido un boton de retroceso que servira para regresar a la landing page

--- javascript/theme.js
+++ javascript/theme.js
@@ -8,13 +8,15 @@ let theme = localStorage.getItem("theme");
 let darkchange = document.querySelector(".modedark");
 let lightchange = document.querySelector(".modelight");
 let logo = document.getElementById("logo_Drag");
-let darklogo = document.getElementById("logo_Drag")
-let back = document.getElementById("logo_Drag")
+let darklogo = document.getElementById("logo_Drag");
+let back = document.getElementById("logo_Drag");
+let backendxd = document.getElementById("retroceder");
 
 darkmode.addEventListener("click", () => {
     document.body.classList.remove("dark-mode"); // activa modo oscuro
     darkmode.src = "../storage/img/luna_prendida.svg";
     sundark.src = "../storage/img/sol_desactivado.svg";
+    backendxd.src = "../storage/img/planeta2.png";
     localStorage.setItem("theme", "dark");
 });
 
@@ -22,15 +24,18 @@ sundark.addEventListener("click", () => {
     document.body.classList.add("dark-mode"); // desactiva modo oscuro
     light.src = "../storage/img/akar-icons_sun-fill.svg";
     moonlight.src = "../storage/img/bi_moon-fill.svg";
+    backendxd.src = "../storage/img/svg1.png";
     localStorage.setItem("theme", "light");
 });
 
 if (theme === "light") {
     darkmode.src = "../storage/img/bi_moon-fill.svg";
     sundark.src = "../storage/img/akar-icons_sun-fill.svg";
+    backendxd.src = "../storage/img/svg1.png";
 } else if (theme === "dark") {
     darkmode.src = "../storage/img/luna_prendida.svg";
     sundark.src = "../storage/img/sol_desactivado.svg";
+    backendxd.src = "../storage/img/planeta2.png";
     document.body.classList.add("dark-mode");
 }
 
