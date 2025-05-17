bb845e1aaed0de0a02505fe7c4c3a4361a974027
Manuel Meneses <manuelmeneses7200@gmail.com>
2025-05-17 15:45:43 -0500

    feat:

--- javascript/main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("otrapagina").addEventListener("click", function() {
-  window.location.href = "../views/information.html"; // Cambia esto si tu archivo tiene otro nombre o ruta
-});
