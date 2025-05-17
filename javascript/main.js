48fc695f629d39bac5ed6aaed079e5c9367edbdb
Manuel Meneses <manuelmeneses7200@gmail.com>
2025-05-17 15:39:52 -0500

    feat: :twisted_rightwards_arrows: Merge Final
    se ha echo cambios y merge de la version final por ahora

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,3 @@
 document.getElementById("otrapagina").addEventListener("click", function() {
-  window.location.href = "pagina1.html"; // Cambia esto si tu archivo tiene otro nombre o ruta
+  window.location.href = "../views/information.html"; // Cambia esto si tu archivo tiene otro nombre o ruta
 });
